8642c90d03781b6bfb7481f49f1a9722dadcf808
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-06-26 12:24:11 +0200

    lint

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,7 +64,7 @@ const logout = async (req, res) => {
   res.json({ message: "Logout success" });
 };
 
-const updateAvatar = async (req, res, next) => {
+const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
 
